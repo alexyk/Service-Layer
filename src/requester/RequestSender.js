a92565dfd508f3aaba016848b29d4b20cd7c18c1
alexyk <alexander.kostadinov@locktrip.com>
2019-08-22 23:38:34 +0300

    Better promise handling for requests - making possible to detect requests that return HTML or text (non-json)

--- src/requester/RequestSender.js
+++ src/requester/RequestSender.js
@@ -1,6 +1,6 @@
-import RequestEndpoints from './RequestEndpoints';
-import RequestMethods from './RequestMethods';
-import RequestParams from './RequestParams';
+import RequestEndpoints from "./RequestEndpoints";
+import RequestMethods from "./RequestMethods";
+import RequestParams from "./RequestParams";
 
 export default class RequestSender {
   constructor(storage, config, headers = {}) {
@@ -10,17 +10,24 @@ export default class RequestSender {
   }
 
   /**
-   * 
-   * @param {String} endpoint 
-   * @param {Enumerator} method 
-   * @param {Object} postObj 
+   *
+   * @param {String} endpoint
+   * @param {Enumerator} method
+   * @param {Object} postObj
    * @param {String} captchaToken
    * @returns {Object}
-   * 
+   *
    */
-  async sendRequest(endpoint, method, postObj = null, captchaToken = null, headers = null) {
+  async sendRequest(
+    endpoint,
+    method,
+    postObj = null,
+    captchaToken = null,
+    headers = null
+  ) {
     const methodRef = this.RequestParams[method].bind(this.RequestParams);
     let requestHeaders = await methodRef(postObj, captchaToken);
+    const _this = this;
 
     if (headers != null) {
       // delete requestHeaders.headers; // 2018-12-20 commented by Alex K - added headers from login as parameter. This particular line is not known what it does.
@@ -28,28 +35,51 @@ export default class RequestSender {
     }
     headers = requestHeaders;
 
-    return fetch(endpoint, headers)
-      .then((response) => {
-        if (!response.ok) {
-          return {
-            body: {},
-            success: response.ok,
-            errors: response.json().then((r) => {
-              this.checkExpiredJwtAndLogOff(r)
-              return r;
-            })
-          };
-        }
+    return fetch(endpoint, headers).then(response => {
+      if (!response.ok) {
         return {
-          body: response.json(),
+          body: {},
           success: response.ok,
-          errors: {}
+          errors: response
+            .text()
+            .then((repsonseAsRawText) => {
+              let jsonData;
+
+              // strip of HTML tags and CSS
+              const regex = /(<([^>]+)>|\{[^\}]+\})/igm;
+              let responseAsPlainText = repsonseAsRawText.replace(regex, '');
+              // strip of new lines
+              responseAsPlainText = responseAsPlainText.replace(/\r?\n|\r/mg, '');
+              // strip of tabs
+              responseAsPlainText = responseAsPlainText.replace(/\t/g, ' ');
+              // strip of more than 2 spaces
+              responseAsPlainText = responseAsPlainText.replace(/ {2,}/g, ' ');
+
+              // try parsing JSON
+              try {
+                jsonData = JSON.parse(repsonseAsRawText)
+              } catch (jsonError) {
+                jsonData = {jsonError, repsonseAsRawText, responseAsPlainText}
+              }
+              _this.checkExpiredJwtAndLogOff(jsonData);
+
+              return jsonData;
+            })
+            .catch(error => error)
         };
-      });
+      }
+
+      return {
+        body: response.json(),
+        success: response.ok,
+        errors: {}
+      };
+    });
   }
 
   checkExpiredJwtAndLogOff(r) {
-    if (r.errors && r.errors.ExpiredJwt) {
+    if ((r.errors && r.errors.ExpiredJwt) || r.jsonError) {
+      // TODO: Remove Async Storage usage
       this._storage.removeItem(`${this.config.domainPrefix}.auth.locktrip`);
       this._storage.removeItem(`${this.config.domainPrefix}.auth.username`);
     }
